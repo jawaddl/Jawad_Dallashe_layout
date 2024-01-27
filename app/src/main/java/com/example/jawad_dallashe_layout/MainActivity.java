48916acda174248f9f1314bf4df6681605adbd8d
DESKTOP-FIA3LA0 <jawaddl2007@Gmail.com>
2024-01-27 22:37:07 +0200

    finish finish

--- app/src/main/java/com/example/jawad_dallashe_layout/MainActivity.java
+++ app/src/main/java/com/example/jawad_dallashe_layout/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.textView6);
+        tv = findViewById(R.id.textView_Dirog);
         tv.setTextSize(16);
     }
 }
