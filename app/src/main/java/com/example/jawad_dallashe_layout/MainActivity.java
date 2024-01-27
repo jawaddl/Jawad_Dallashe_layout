d5e231ce44cfeb3720ab5832573cfa87fcc7cdfb
DESKTOP-FIA3LA0 <jawaddl2007@Gmail.com>
2024-01-27 22:08:19 +0200

    commit 3

--- app/src/main/java/com/example/jawad_dallashe_layout/MainActivity.java
+++ app/src/main/java/com/example/jawad_dallashe_layout/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.jawad_dallashe_layout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textView6);
+        tv.setTextSize(16);
     }
 }
